cb272416cd7504e79c91c00e9206b1bd8759c35f
Digvijay Singh <digvijay.singh@insurancedekho.com>
2021-04-17 15:43:00 +0530

    small fix

--- routes/users.js
+++ routes/users.js
@@ -123,8 +123,7 @@ router.post('/login', async (req, res) => {
       res.render('login', { errors });
     }
     console.log("going to query pool");
-    const client = await poolConn.connect();
-    const loginResult = await client.query('SELECT Id, sfid, Name, email,PM_email__c FROM salesforce.Contact WHERE email = $1 AND password2__c = $2', [email, password]);
+    const loginResult = await poolConn.query('SELECT Id, sfid, Name, email,PM_email__c FROM salesforce.Contact WHERE email = $1 AND password2__c = $2', [email, password]);
     if (loginResult.rowCount > 0) {
       userId = loginResult.rows[0].sfid;
       objUser = loginResult.rows[0];
@@ -134,13 +133,12 @@ router.post('/login', async (req, res) => {
     }
     console.log("objuser 1 -> ", objUser);
 
-    const teamQueryResult = await client.query('SELECT sfid FROM salesforce.Team__c WHERE Manager__c =  $1 ', [userId]);
+    const teamQueryResult = await poolConn.query('SELECT sfid FROM salesforce.Team__c WHERE Manager__c =  $1 ', [userId]);
     if (teamQueryResult.rowCount > 0) {
       objUser.isManager = true;
     } else {
       objUser.isManager = false; 
     }
-    client.release();
 
     console.log("objuser 2 -> ", objUser);
 
