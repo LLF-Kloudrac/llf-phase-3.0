4db6af7bb45cb9feb5f2af6af5c74952a6c65b1e
Digvijay Singh <digvijay.singh@insurancedekho.com>
2021-04-17 16:50:14 +0530

    check postgres conn on server start

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,22 +1,32 @@
-const {Pool,Client} = require('pg')
+const { Pool, Client } = require('pg')
 
-const dbConfig = {
-    connectionString: process.env.DATABASE_URL,
-    ssl:true,
-  }
+const dbConn = {};
 
-console.log('process.env.DATABASE_URL: '+process.env.DATABASE_URL);
-
-const pool = new Pool(dbConfig);
-pool.on('error',function(err){
-    console.log('Error Message'+err.message);
-    console.log('Error Stack'+err.stack);
-})
-
-
-module.exports = {
-    pool,
-    query: (text, params, callback) =>{
-        return pool.query(text, params, callback)
+dbConn.connect = function connect(cb) {
+    console.log("db conn called");
+    const dbConfig = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
     }
+
+    console.log('process.env.DATABASE_URL: ' + process.env.DATABASE_URL);
+
+    const pool = new Pool(dbConfig);
+    pool.on('error', function (err) {
+        console.log('Error Message' + err.message);
+        console.log('Error Stack' + err.stack);
+        return cb(false);
+    })
+
+    dbConn.pool = pool;
+    return cb(true);
 }
+
+module.exports = dbConn;
+
+// module.exports = {
+//     pool,
+//     query: (text, params, callback) =>{
+//         return pool.query(text, params, callback)
+//     }
+// }
